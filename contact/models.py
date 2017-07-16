da6e629c7d8393c893112265ec925e7bfee18f26
munnu <moniqueblake4@gmail.com>
2017-07-15 19:42:08 -0700

    Overrode wagtail send_mail function to process emails better

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from wagtail.wagtailadmin.utils import send_mail as wagtailadmin_send_mail
+from django.core.mail import send_mail as django_send_mail
+
+
 from wagtailcaptcha.models import WagtailCaptchaEmailForm
 from modelcluster.fields import ParentalKey
 from wagtail.wagtailadmin.edit_handlers import (
@@ -31,3 +35,26 @@ class FormPage(WagtailCaptchaEmailForm):
             FieldPanel('subject'),
         ], "Email"),
     ]
+
+
+    def __init__(self, *args, **kwargs):
+        super(FormPage, self).__init__(*args, **kwargs)
+
+    def send_mail(self, form):
+        addresses = [x.strip() for x in self.to_address.split(',')]
+        content = []
+        for field in form:
+            value = field.value()
+            if isinstance(value, list):
+                value = ', '.join(value)
+            if str(field.label) == 'Subject':
+                self.subject = value
+                continue
+            elif str(field.label) == 'Email':
+                self.from_address = value
+                continue
+            content.append('{}: {}'.format(field.label, value))
+        content = '\n'.join(content)
+        # wagtailadmin_send_mail(self.subject, content, addresses, from_email=self.from_address,)
+        # bypassing wagtail's send_mail function for now, due to its conditional statements on defaulting 'from' email
+        django_send_mail(self.subject, content, from_email=self.from_address, recipient_list=[self.to_address])
