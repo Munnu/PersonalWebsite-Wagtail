8812bb1a2ceefd73526a9368d43a7df3c094c8d1
munnu <moniqueblake4@gmail.com>
2017-07-20 02:28:10 -0700

    Created a resume and about app, though redundant in structure

---
+++ MyWagtailWebsite/resume/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailsearch import index
+
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
+# Create your models here.
+class ResumePage(Page):
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
