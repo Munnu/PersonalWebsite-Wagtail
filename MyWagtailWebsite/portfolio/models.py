fdcb90bf4af547d9400301f393310b7712b705b2
munnu <moniqueblake4@gmail.com>
2017-07-22 14:57:59 -0700

    Fleshing out the portfolio, mostly functional, need minor adds.

--- MyWagtailWebsite/portfolio/models.py
+++ MyWagtailWebsite/portfolio/models.py
@@ -1,36 +1,84 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
 from django.db import models
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailsearch import index
 
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import (FieldPanel,
+                                                InlinePanel,
+                                                MultiFieldPanel
+                                                )
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsnippets.models import register_snippet
 
 # # Create your models here.
-# class PortfolioPage(Page):
-#     intro = RichTextField(blank=True)
-#
-#     content_panels = Page.content_panels + [
-#         FieldPanel('intro', classname="full")
-#     ]
+class PortfolioPage(Page):
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(PortfolioPage, self).get_context(request)
+        portfolio_item_pages = self.get_children().live().order_by('-first_published_at')
+        context['portfolio_item_pages'] = portfolio_item_pages
+        return context
+
 
 class PortfolioItemPage(Page):
-    # date = models.DateField("Post date")
-    # intro = models.CharField(max_length=250)
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-    # categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
+    categories = ParentalManyToManyField('portfolio.PortfolioCategory', blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
 
     search_fields = Page.search_fields + [
+        index.SearchField('intro'),
         index.SearchField('body'),
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        ], heading="Portfolio item full description"),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
     ]
+
+class PortfolioItemPageGalleryImage(Orderable):
+    page = ParentalKey(PortfolioItemPage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+
+@register_snippet
+class PortfolioCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('name'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'portfolio categories'
