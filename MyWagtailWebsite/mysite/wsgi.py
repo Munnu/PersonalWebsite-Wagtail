967761bff9cb578e241dda3f387b7e161745a2b4
munnu <moniqueblake4@gmail.com>
2017-09-09 23:52:46 -0700

    Added dj_static info to wsgi file

--- MyWagtailWebsite/mysite/wsgi.py
+++ MyWagtailWebsite/mysite/wsgi.py
@@ -12,7 +12,9 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+application = Cling(get_wsgi_application())
