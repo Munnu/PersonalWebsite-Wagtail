a9f3c179885752639cc6e9f0e39493afe058945d
Munnu <moniqueblake4@gmail.com>
2019-03-31 22:54:39 -0700

    adding logging in settings, disabling css compression

--- MyWagtailWebsite/mysite/settings/production.py
+++ MyWagtailWebsite/mysite/settings/production.py
@@ -37,13 +37,42 @@ SECURE_SSL_REDIRECT = False
 # Compress static files offline
 # http://django-compressor.readthedocs.org/en/latest/settings/#django.conf.settings.COMPRESS_OFFLINE
 
-COMPRESS_OFFLINE = True
-COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = False
+COMPRESS_ENABLED = False
 
 COMPRESS_CSS_FILTERS = [
         'compressor.filters.css_default.CssAbsoluteFilter',
             'compressor.filters.cssmin.CSSMinFilter',
             ]
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 
 
 try:
